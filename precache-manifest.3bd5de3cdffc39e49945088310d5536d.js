afbf7bf48cc7a5363c4cd3ad569a63eb59865442
Charles <charles.neblett@gmail.com>
2019-08-06 11:25:25 -0500

    Updates

---
+++ precache-manifest.3bd5de3cdffc39e49945088310d5536d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da9f24310661f844cff124455054b3f3",
+    "url": "/imageRecognizer/index.html"
+  },
+  {
+    "revision": "c1a55c5209dfde3117e5",
+    "url": "/imageRecognizer/static/css/main.053f6710.chunk.css"
+  },
+  {
+    "revision": "9576736282b9006ccc23",
+    "url": "/imageRecognizer/static/js/2.b524c539.chunk.js"
+  },
+  {
+    "revision": "c1a55c5209dfde3117e5",
+    "url": "/imageRecognizer/static/js/main.5eb9b062.chunk.js"
+  },
+  {
+    "revision": "93ec9fc479387c004cba",
+    "url": "/imageRecognizer/static/js/runtime~main.93f0e3dc.js"
+  }
+]);
